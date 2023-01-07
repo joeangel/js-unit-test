8fdd6b13c8e0cf9fe587e82f8729749dce950942
JoeAngel <joe10330@gmail.com>
2023-01-07 15:23:38 +0800

    refactor: use jest for test

--- test/authentication.test.js
+++ test/authentication.test.js
@@ -1,26 +1,24 @@
 import {Authentication} from "../src/authentication";
+import * as Otp from "../src/otp";
+import {Profile} from "../src/profile";
 
+jest.mock("../src/otp");
+jest.mock("../src/profile");
 describe('authenticate account is valid', function () {
-    let authentication = new Authentication();
-
-    beforeEach( function () {
-        authentication = new Authentication();
-    });
-
     it('should be valid', () => {
-        AuthenticationForTest();
+        let authentication = new Authentication();
         const expected = true;
 
+        Profile.mockImplementationOnce(() => {
+            return {
+                get_password: () => {
+                    return "91";
+                }
+            };
+        });
+        Otp.get_token.mockReturnValueOnce('000000');
         let result = authentication.is_valid('joey', '91000000');
 
         expect(result).toBe(expected);
     });
-
-    function AuthenticationForTest() {
-        authentication.getPassword = (account) => {
-            if (account === 'joey') return '91';
-            return '';
-        };
-        authentication.getToken = () => '000000';
-    }
 });
