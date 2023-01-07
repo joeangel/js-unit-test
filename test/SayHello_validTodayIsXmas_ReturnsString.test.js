cb30261c5e5e189b40d08b80419a05426f17b89e
JoeAngel <joe10330@gmail.com>
2023-01-07 11:29:29 +0800

    fix: use xmas replace holiday

--- test/SayHello_validTodayIsXmas_ReturnsString.test.js
+++ test/SayHello_validTodayIsXmas_ReturnsString.test.js
@@ -1,7 +1,7 @@
 
 import {Holiday} from "../src/Holyday";
 describe('say hello', function () {
-    it('today is holiday', function () {
+    it('today is xmas', function () {
         let holiday = HolidayForTest(12, 25);
         const expected = "Merry Xmas";
 
@@ -9,7 +9,7 @@ describe('say hello', function () {
 
         expect(result).toBe(expected);
     });
-    it('today is not holiday', function () {
+    it('today is not xmas', function () {
         let holiday = HolidayForTest(12,24)
         const expected = "Today is not Xmas";
 
