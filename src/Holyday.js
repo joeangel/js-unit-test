33a951272f830b5bedda7b6ba6e894cd2dfacbfa
JoeAngel <joe10330@gmail.com>
2023-01-07 11:07:22 +0800

    test: passed say hello

--- src/Holyday.js
+++ src/Holyday.js
@@ -1,14 +1,18 @@
 export class Holiday {
     sayHello() {
-        const now = new Date();
+        const today = this.getToday();
         let isXmas = false;
 
         // Compare Xmas date: 12/25
-        if (now.getMonth() === (12 - 1) && now.getDate() === 25) {
+        if (today.getMonth() === (11) && today.getDate() === 25) {
             isXmas = true;
         }
 
         if (isXmas) return "Merry Xmas";
         return "Today is not Xmas";
+    }
+
+    getToday() {
+        return new Date();
     }
 }
--- test/SayHello_validTodayIsXmas_ReturnsString.test.js
+++ test/SayHello_validTodayIsXmas_ReturnsString.test.js
@@ -2,19 +2,30 @@
 import {Holiday} from "../src/Holyday";
 describe('say hello', function () {
     it('today is holiday', function () {
-        let holiday = makeHoliday();
+        let holiday = HolidayForTest(12, 25);
         const expected = "Merry Xmas";
+
         let todayIsHoliday = holiday.sayHello();
+
         expect(todayIsHoliday).toBe(expected);
     });
     it('today is not holiday', function () {
-        let holiday = makeHoliday()
+        let holiday = HolidayForTest(12,24)
         const expected = "Today is not Xmas";
+
         let todayIsHoliday = holiday.sayHello();
+
         expect(todayIsHoliday).toBe(expected);
     });
 
-    function makeHoliday() {
-        return new Holiday();
+    function HolidayForTest(month, date) {
+        let holiday = new Holiday();
+        holiday.getToday = () => {
+            let today = new Date();
+            today.setMonth(month - 1);
+            today.setDate(date);
+            return today;
+        }
+        return holiday;
     }
 });
